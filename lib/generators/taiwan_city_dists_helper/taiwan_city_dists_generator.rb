90abe06b5a8fefb0ed6f361efe636adab3c6e5a8
unayung <Unayung@gmail.com>
2013-02-28 01:52:55 +0800

    rename

---
+++ lib/generators/taiwan_city_dists_helper/taiwan_city_dists_generator.rb
@@ -0,0 +1,17 @@
+require 'rails/generators'
+#require 'rails/generators/active_record/migration'
+
+# module TaiwanCityDistsHelper
+#   module Generators
+    class TaiwanCityDistsGenerator < Rails::Generators::Base
+      include Rails::Generators::Migration
+      extend ActiveRecord::Generators::Migration
+
+      self.source_paths << File.join(File.dirname(__FILE__), 'templates')
+
+      def create_migration_file
+        migration_template "migration.rb", "db/migrate/create_cities_and_dists.rb"
+      end
+    end
+#   end
+# end
