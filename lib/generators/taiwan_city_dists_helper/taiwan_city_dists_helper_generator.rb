e805cb4421f318e80bb187d9bc4ea01667f7cf67
unayung <Unayung@gmail.com>
2013-02-28 10:12:22 +0800

    moving class

--- lib/generators/taiwan_city_dists_helper/taiwan_city_dists_helper_generator.rb
+++ lib/generators/taiwan_city_dists_helper/taiwan_city_dists_helper_generator.rb
@@ -1,17 +1,22 @@
 require 'rails/generators'
-#require 'rails/generators/active_record/migration'
+require 'rails/generators/migration'
+require 'rails/generators/active_record/migration'
 
 # module TaiwanCityDistsHelper
 #   module Generators
-    class TaiwanCityDistsHelperGenerator < Rails::Generators::Base
-      include Rails::Generators::Migration
-      extend ActiveRecord::Generators::Migration
+class TaiwanCityDistsHelperGenerator < Rails::Generators::Base
+  include Rails::Generators::Migration
+  #extend ActiveRecord::Generators::Migration
 
-      self.source_paths << File.join(File.dirname(__FILE__), 'templates')
+  self.source_paths << File.join(File.dirname(__FILE__), 'templates')
 
-      def create_migration_file
-        migration_template "migration.rb", "db/migrate/create_cities_and_dists.rb"
-      end
-    end
+  def create_migration_file
+    migration_template "migration.rb", "db/migrate/create_cities_and_dists.rb"
+  end
+
+  def self.next_migration_number(path)
+    @migration_number = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i.to_s
+  end
+end
 #   end
 # end
