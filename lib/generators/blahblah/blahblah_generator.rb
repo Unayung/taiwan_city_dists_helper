c8b6c55eed6e2c826a921fb3c903dcc2c117e5de
unayung <Unayung@gmail.com>
2013-03-01 13:02:50 +0800

    module test

--- lib/generators/blahblah/blahblah_generator.rb
+++ lib/generators/blahblah/blahblah_generator.rb
@@ -2,8 +2,10 @@ require 'rails/generators'
 require 'rails/generators/migration'
 require 'rails/generators/active_record/migration'
 
-class BlahblahGenerator < Rails::Generators::Base
-  include Rails::Generators::Migration
+module TaitanCityDistsHelper
+  class BlahblahGenerator < Rails::Generators::Base
+    include Rails::Generators::Migration
 
-  puts "Just blahblah"
+    puts "Just blahblah"
+  end
 end
