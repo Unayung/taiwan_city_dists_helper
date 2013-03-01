31e19a76ce0dcf0163627ffde73b906177395f51
unayung <Unayung@gmail.com>
2013-03-02 00:30:34 +0800

    add js

--- lib/taiwan_city_dists_helper/helper.rb
+++ lib/taiwan_city_dists_helper/helper.rb
@@ -1,13 +1,36 @@
 module TaiwanCityDistsHelper
   module Helper
-    def render_city_select(f, cities)
+    def render_city_select(f, cities, blank=false)
       #select_tag "cities", options_from_collection_for_select(cities, "id", "name")
-      f.collection_select :city_id, cities, :id, :name, include_blank: true
+      f.collection_select :city_id, cities, :id, :name, include_blank: blank
     end
 
-    def render_dist_select(f, cities, dists)
+    def render_grouped_dist_select(f, cities, blank=false)
       #select_tag "dists", grouped_options_for_select(dists, "id", "name")
-      f.grouped_collection_select :dist_id, cities, :dists, :name, :id, :name, include_blank: true
+      f.grouped_collection_select :dist_id, cities, :dists, :name, :id, :name, include_blank: blank
+    end
+
+    def render_dist_select(f, dists, blank=false)
+      f.collection_select :dist_id, dists, :id, :name, include_blank: blank
+    end
+
+    def render_city_dists_js
+      s = <<-HEREDOC
+      jQuery ->
+        $('#person_state_id').parent().hide()
+        states = $('#person_state_id').html()
+        $('#person_country_id').change ->
+          country = $('#person_country_id :selected').text()
+          escaped_country = country.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1')
+          options = $(states).filter("optgroup[label=#{escaped_country}]").html()
+          console.log(options)
+          if options
+            $('#person_state_id').html(options)
+            $('#person_state_id').parent().show()
+          else
+            $('#person_state_id').empty()
+            $('#person_state_id').parent().hide()
+      HEREDOC
     end
   end
 end
