485d670bba084fb1a7ec49b5827727b763dea9fc
unayung <Unayung@gmail.com>
2013-03-02 00:19:16 +0800

    helper

--- lib/taiwan_city_dists_helper/helper.rb
+++ lib/taiwan_city_dists_helper/helper.rb
@@ -1,11 +1,13 @@
 module TaiwanCityDistsHelper
   module Helper
-    def render_city_select
-      select_tag "cities", options_from_collection_for_select(@cities, "id", "name")
+    def render_city_select(f, cities)
+      #select_tag "cities", options_from_collection_for_select(cities, "id", "name")
+      f.collection_select :city_id, cities, :id, :name, include_blank: true
     end
 
-    def render_dist_select
-      select_tag "dists", options_from_collection_for_select(@dists, "id", "name")
+    def render_dist_select(f, cities, dists)
+      #select_tag "dists", grouped_options_for_select(dists, "id", "name")
+      f.grouped_collection_select :dist_id, cities, :dists, :name, :id, :name, include_blank: true
     end
   end
 end
