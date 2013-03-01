6742b63266c99250c2b336ba91bd006cc4dc212a
unayung <Unayung@gmail.com>
2013-03-01 20:21:07 +0800

    test

--- lib/generators/taiwan_city_dists_helper/blahblah_generator.rb
+++ lib/generators/taiwan_city_dists_helper/blahblah_generator.rb
@@ -5,7 +5,7 @@ require 'rails/generators/active_record/migration'
 module TaitanCityDistsHelper
   module Generators
     class BlahblahGenerator < Rails::Generators::Base
-      include Rails::Generators::Migration
+      #include Rails::Generators::Migration
 
       puts "Just blahblah"
     end
