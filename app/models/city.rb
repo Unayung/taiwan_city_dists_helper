2ddd8262917e70ae6c0ef26b4ce908ba4f7eb205
unayung <Unayung@gmail.com>
2013-05-02 00:24:30 +0800

    fix protected attributes issue

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
 class City < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :name
   has_many :dists
 end
--- app/models/dist.rb
+++ app/models/dist.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
 class Dist < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :name, :city_id
   belongs_to :city
 end
