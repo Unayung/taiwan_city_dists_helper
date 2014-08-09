887d927b7497e438a7722c57a2f0071d8829bbe5
Unayung Chen <unayung@gmail.com>
2014-08-09 17:10:43 +0800

    remove attr_accessor

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,4 @@
 # -*- encoding : utf-8 -*-
 class City < ActiveRecord::Base
-  attr_accessor :name
   has_many :dists
 end
--- app/models/dist.rb
+++ app/models/dist.rb
@@ -1,5 +1,4 @@
 # -*- encoding : utf-8 -*-
 class Dist < ActiveRecord::Base
-  attr_accessor :name, :city_id
   belongs_to :city
 end
