150d74b9a1cc6d0488030c7dddb9c96c57b79249
Unayung Chen <unayung@gmail.com>
2014-08-19 23:33:34 +0800

    add html_option to helpers

--- lib/taiwan_city_dists_helper/helper.rb
+++ lib/taiwan_city_dists_helper/helper.rb
@@ -1,15 +1,15 @@
 module TaiwanCityDistsHelper
   module Helper
-    def render_city_select(f, cities, blank=false)
-      f.collection_select :city_id, cities, :id, :name, include_blank: blank
+    def render_city_select(f, cities, blank=false, html_options = {})
+      f.collection_select(:city_id, cities, :id, :name, include_blank: blank, html_options)
     end
 
-    def render_grouped_dist_select(f, cities, blank=false)
-      f.grouped_collection_select :dist_id, cities, :dists, :name, :id, :name, include_blank: blank
+    def render_grouped_dist_select(f, cities, blank=false, html_options = {})
+      f.grouped_collection_select(:dist_id, cities, :dists, :name, :id, :name, include_blank: blank, html_options)
     end
 
-    def render_dist_select(f, dists, blank=false)
-      f.collection_select :dist_id, dists, :id, :name, include_blank: blank
+    def render_dist_select(f, dists, blank=false, html_options = {})
+      f.collection_select(:dist_id, dists, :id, :name, include_blank: blank, html_options)
     end
 
   end
